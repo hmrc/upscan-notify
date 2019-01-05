132e387cf98015c435644e0b93ea0a1214d76e81
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2019-01-05 11:10:26 +0000

    Updated copyrights

--- app/services/MessageParser.scala
+++ app/services/MessageParser.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 HM Revenue & Customs
+ * Copyright 2019 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,6 @@
 package services
 
 import model.{FileUploadEvent, Message}
-
-import scala.concurrent.Future
 
 trait MessageParser[F[_]] {
 
