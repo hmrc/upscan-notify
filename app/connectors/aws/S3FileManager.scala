a75202009cdeb7e32eacba227ec7b4bbc0a904ed
Tim Golding <cybersaurus@users.noreply.github.com>
2019-02-22 16:45:04 +0000

    PLATOPS-1900: Further WIP to refactor retrieveReadyMetadata into a higher-order function that accepts an extracter and assembler.

--- app/connectors/aws/S3FileManager.scala
+++ app/connectors/aws/S3FileManager.scala
@@ -24,7 +24,6 @@ import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.ObjectMetadata
 import model.{FileReference, InvalidUploadDetails, RequestContext, S3ObjectLocation, UploadDetails, ValidUploadDetails}
 import org.apache.commons.io.IOUtils
-import play.api.Logger
 import services._
 import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext.fromLoggingDetails
 import util.logging.LoggingDetails
@@ -35,7 +34,7 @@ import scala.util.{Failure, Success, Try}
 
 class S3FileManager @Inject()(s3Client: AmazonS3, downloadUrlGenerator: DownloadUrlGenerator) extends FileManager {
 
-
+/*
   private def retrieveMetadata(userMetadata: S3ObjectMetadata) : Try[services.UploadedFileMetadata] = {
     for {
       callbackUrl      <- retrieveCallbackUrl(userMetadata)
@@ -45,7 +44,134 @@ class S3FileManager @Inject()(s3Client: AmazonS3, downloadUrlGenerator: Download
     } yield services.UploadedFileMetadata(fileReference, callbackUrl, uploadDetails, userMetadata.underlying.getContentLength,
       requestContext, userMetadata.userMetadata)
   }
+*/
+/*
+  private def _retrieveMetadata[E](userMetadata: S3ObjectMetadata, block: S3ObjectMetadata => Future[E]) : Future[(services.UploadedFileMetadata, E)] = {
+    for {
+      callbackUrl      <- Future.fromTry(retrieveCallbackUrl(userMetadata))
+      fileReference    <- Future.fromTry(userMetadata.get("file-reference", FileReference.apply))
+      uploadDetails    <- Future.fromTry(retrieveUploadDetails(userMetadata))
+      requestContext   <- Future.fromTry(retrieveUserContext(userMetadata))
+      enriched         <- block(userMetadata)
+    } yield (services.UploadedFileMetadata(fileReference, callbackUrl, uploadDetails, userMetadata.underlying.getContentLength,
+      requestContext, userMetadata.userMetadata), enriched)
+  }
+*/
+  /**
+    * Retrieve the common UploadedFileMetadata value.
+    * Also extract use-case specific fields, such as downloadUrl, by delegating to the supplied `extracter`.
+    * Finally assemble both of the above into a model, by delegating to the supplied `assembler`, and return it.
+    *
+    * @param userMetadata
+    * @param extracter extracts use-case specific fields that will be included in the returned model.
+    *              e.g. downloadUrl (for ready path) or errorMessage (for quarantined path).
+    * @param assembler function to assemble the common UploadedFileMetadata and use-case specific fields
+    *                  (e.g. downloadUrl) into the final model value returned from this method.
+    * @tparam E the extracted data that will be assembled into the returned model, M, along with the common UploadedFileMetadata values.
+    * @tparam M the returned model, which wraps UploadedFileMetadata with the use-case specific extracted fields from above.
+    * @return the model encapsulating common fields in UploadedFileMetadata, and use-case specific fields, e.g. downloadUrl.
+    */
+  private def __retrieveMetadata[E,M](userMetadata: S3ObjectMetadata,
+                                      extracter: S3ObjectMetadata => Future[E],
+                                      assembler: (services.UploadedFileMetadata, E) => M) : Future[M] = {
+    for {
+      callbackUrl      <- Future.fromTry(retrieveCallbackUrl(userMetadata))
+      fileReference    <- Future.fromTry(userMetadata.get("file-reference", FileReference.apply))
+      uploadDetails    <- Future.fromTry(retrieveUploadDetails(userMetadata))
+      requestContext   <- Future.fromTry(retrieveUserContext(userMetadata))
+      extracted        <- extracter(userMetadata)
+    } yield {
+      assembler(
+        services.UploadedFileMetadata(
+          fileReference, callbackUrl, uploadDetails, userMetadata.underlying.getContentLength,
+          requestContext, userMetadata.userMetadata
+        ),
+        extracted)
+    }
+  }
 
+  private def assembleUploadedFileMetadataAndDetails[T,R](objectLocation: S3ObjectLocation,
+                                                          extractor: S3ObjectMetadata => Future[T],
+                                                          assembler: (services.UploadedFileMetadata, T) => R) : Future[R] = {
+    implicit val ld = LoggingDetails.fromS3ObjectLocation(objectLocation)
+
+    for {
+      metadata  <- Future(s3Client.getObjectMetadata(objectLocation.bucket, objectLocation.objectKey))
+      model     <- __retrieveMetadata(S3ObjectMetadata(metadata, objectLocation), extractor, assembler)
+
+    } yield model
+  }
+
+  override def retrieveReadyMetadata(objectLocation: S3ObjectLocation): Future[UploadedFileMetadataWithDownloadUrl] = {
+
+    def extractDownloadUrl(s3Metadata: S3ObjectMetadata): Future[URL] = {
+      Future.fromTry {
+        for {
+          consumingService <- retrieveConsumingService(s3Metadata)
+        } yield downloadUrlGenerator.generate(objectLocation, consumingService)
+      }
+    }
+
+    def assembler(common: services.UploadedFileMetadata, downloadUrl: URL) = UploadedFileMetadataWithDownloadUrl(downloadUrl, common)
+
+    assembleUploadedFileMetadataAndDetails(objectLocation, extractDownloadUrl, assembler)
+  }
+
+  override def retrieveFailedObject(objectLocation: S3ObjectLocation): Future[UploadedFileMetadataWithError] = {
+
+    def extractErrorMessage(s3Metadata: S3ObjectMetadata): Future[String] = {
+      implicit val ld = LoggingDetails.fromS3ObjectLocation(objectLocation)
+
+      for {
+        s3Object     <- Future(s3Client.getObject(objectLocation.bucket, objectLocation.objectKey))
+        errorMessage <- Future.fromTry(Try[String](IOUtils.toString(s3Object.getObjectContent)))
+      } yield errorMessage
+    }
+
+    def assembler(common: services.UploadedFileMetadata, errorMessage: String) = UploadedFileMetadataWithError(errorMessage, common)
+
+    assembleUploadedFileMetadataAndDetails(objectLocation, extractErrorMessage, assembler)
+  }
+/*
+  override def retrieveReadyMetadata(objectLocation: S3ObjectLocation): Future[UploadedFileMetadataWithDownloadUrl] = {
+    implicit val ld = LoggingDetails.fromS3ObjectLocation(objectLocation)
+
+    def extractDownloadUrl(s3Metadata: S3ObjectMetadata): Future[URL] = {
+      Future.fromTry {
+        for {
+          consumingService <- retrieveConsumingService(s3Metadata)
+        } yield downloadUrlGenerator.generate(objectLocation, consumingService)
+      }
+    }
+
+    for {
+      metadata          <- Future(s3Client.getObjectMetadata(objectLocation.bucket, objectLocation.objectKey))
+      (common,enriched) <- _retrieveMetadata(S3ObjectMetadata(metadata, objectLocation), extractDownloadUrl)
+    } yield {
+      UploadedFileMetadataWithDownloadUrl(enriched, common)
+    }
+  }
+*/
+  /*
+  override def retrieveFailedObject(objectLocation: S3ObjectLocation): Future[UploadedFileMetadataWithError] = {
+    implicit val ld = LoggingDetails.fromS3ObjectLocation(objectLocation)
+
+    def extractErrorMessage(s3Metadata: S3ObjectMetadata): Future[String] = {
+      for {
+        s3Object     <- Future(s3Client.getObject(objectLocation.bucket, objectLocation.objectKey))
+        errorMessage <- Future.fromTry(Try[String](IOUtils.toString(s3Object.getObjectContent)))
+      } yield errorMessage
+    }
+
+    for {
+      metadata          <- Future(s3Client.getObjectMetadata(objectLocation.bucket, objectLocation.objectKey))
+      (common,enriched) <- _retrieveMetadata(S3ObjectMetadata(metadata, objectLocation), extractErrorMessage)
+    } yield {
+      UploadedFileMetadataWithError(enriched, common)
+    }
+  }
+  */
+/*
   override def retrieveReadyMetadata(objectLocation: S3ObjectLocation): Future[UploadedFileMetadataWithDownloadUrl] = {
     implicit val ld = LoggingDetails.fromS3ObjectLocation(objectLocation)
 
@@ -72,7 +198,7 @@ class S3FileManager @Inject()(s3Client: AmazonS3, downloadUrlGenerator: Download
       UploadedFileMetadataWithError(content, fileMetadata)
     }
   }
-
+*/
   private def retrieveCallbackUrl(metadata: S3ObjectMetadata): Try[URL] =
     metadata.get("callback-url", { new URL(_) })
 
